ee6d0d4e211f713441f32009bed6ddfa0783005f
Jin Hong <jnhg1016@gmail.com>
2021-05-11 23:33:28 +1000

    api post function to write all notes to json file changed

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const path = require("path");
 // Require fs to read and write to files
 const fs = require('fs');
+const notesData = require("./db/db.json");
 
 // Creates express server and sets initial port
 const app = express();
@@ -14,83 +15,69 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './public')));
 
-//  Initialize addedNote
-let addedNote = [];
+module.exports = function(app) {
+	function writeToDB(notes){
+        // Converts new JSON Array back to string
+        notes = JSON.stringify(notes);
+        console.log (notes);
+        // Writes String back to db.json
+        fs.writeFileSync("./db/db.json", notes, function(err){
+            if (err) {
+                return console.log(err);
+            }
+        });
+	}
+}
 
 // API call response for all inputted notes, and send results to the browser as an array of object
 app.get("/api/notes", function (req, res) {
-	try {
-		// reads the notes from the json file
-		addedNote = fs.readFileSync("./db/db.json", "utf8");
-		console.log("Note saved.");
-		// parse the data to get an array of objects
-		addedNote = JSON.parse(addedNote);
-
-		// handles errors
-		} catch (err) {
-			console.log("\n error (in app.get.catch):");
-			console.log(err);
-		}
-
-		// send objects to the browser
-		res.json(addedNote);
+	res.json(notesData);
 });
 
-// API to write all newly inputted notes to the json file
+// API to write all newly inputted notes to JSON file
 app.post("/api/notes", function (req, res) {
-	try {
-		// reads the json file
-		addedNote = fs.readFileSync("./db/db.json", "utf8");
-		console.log(addedNote);
-		// parse the data to get an array of objects
-		addedNote = JSON.parse(addedNote);
-		// set new notes by using id
-		req.body.id = addedNote.length;
-		// add new notes to the array of objects
-		addedNote.push(req.body);
-		// make it string(stringify) to write to json file
-		addedNote = JSON.stringify(addedNote);
-		// writes new notes to jason file
-		fs.writeFile("./db/db.json", addedNote, "utf8", function(err) {
-		// handles errors
-		if (err) throw err;
-		});
-		// change data back to an array of objects
-		res.json(JSON.parse(addedNote));
+
+    // Set unique id to entry
+	if (notesData.length == 0){
+		req.body.id = "0";
+	} else {
+		req.body.id = JSON.stringify(JSON.parse(notesData[notesData.length - 1].id) + 1);
+	}
 	
-		// handles errors
-		} catch (err) {
-		throw err;
-		console.error(err);
-		}
+	console.log("req.body.id: " + req.body.id);
+
+	// Pushes Body to JSON array
+	notesData.push(req.body);
+
+	// Write notes data to database
+	writeToDB(notesData);
+	console.log(notesData);
+
+	// Returns new note in JSON file
+	res.json(req.body);
 });
 
 // API to delete individual notes from the json file
 app.delete('/api/notes/:id', function (req, res) {
-	try {
-		// reads the json file
-		addedNote = fs.readFileSync("./db/db.json", "utf8");
-		// parse data to get an array of the objects
-		addedNote = JSON.parse(addedNote);
-		// delete selected note from the array
-		addedNote = addedNote.filter(function(note) {
-		return note.id != req.params.id;
-		});
-		// make it string(stringify) to write to json file
-		addedNote = JSON.stringify(addedNote);
-		// write new notes to the file
-		fs.writeFile("./db/db.json", addedNote, "utf8", function(err) {
-		// error handling
-		if (err) throw err;
-		});
-		// change data back to an array of objects
-		res.send(JSON.parse(addedNote));
+	let deleteId = req.params.id;
+	console.log(req.params.id);
 	
-		// handles errors
-		} catch (err) {
-		throw err;
-		console.error(err);
-		}
+	fs.readFile('./db/db.json', 'utf8', (err, data) => {
+		let dataArray = JSON.parse(data);
+
+		dataArray = dataArray.filter(function (note) {
+			return note.id != deleteId;
+		});
+
+		let newDataString = JSON.stringify(dataArray);
+
+		fs.writeFile('./db/db.json', newDataString, function (err) {
+			if (err) throw err;
+			console.log('Note deleted.');
+		});
+	});
+
+	res.sendFile(path.join(__dirname, './db/db.json'))
 });
 
 // Get homepage when the 'GetStarted' button is clicked
@@ -101,10 +88,6 @@ app.get("/notes", function (req, res) {
 // If no matching route is found default to homepage
 app.get("*", function (req, res) {
 	res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/api/notes", function(req, res) {
-	return res.sendFile(path.json(__dirname, "./db/db.json"));
 });
 
 // Start the server on the port
