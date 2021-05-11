c58cedf38b9c55bf98de7cd75edd5334b1734505
Jin Hong <jnhg1016@gmail.com>
2021-05-11 23:58:57 +1000

    debugging in html.js to find html in public directory

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -5,11 +5,11 @@ module.exports = function(app) {
 
     // Get homepage when the 'GetStarted' button is clicked
     app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
     // If no matching route is found default to homepage
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 }
